34b19438549ad1430d203ae36241d8f69e50a295
regiuss <regiuslink@gmail.com>
2022-10-24 13:29:42 +0300

    add FXMLLoader

--- src/main/java/ru/regiuss/dxf/selection/helper/App.java
+++ src/main/java/ru/regiuss/dxf/selection/helper/App.java
@@ -1,6 +1,9 @@
 package ru.regiuss.dxf.selection.helper;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -9,6 +12,11 @@ public class App extends Application {
         stage.setTitle("DXFSelectionHelper");
         stage.setMinWidth(800);
         stage.setMinHeight(600);
+        stage.setWidth(800);
+        stage.setHeight(600);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main.fxml"));
+        Parent parent = loader.load();
+        stage.setScene(new Scene(parent));
         stage.show();
     }
 }
