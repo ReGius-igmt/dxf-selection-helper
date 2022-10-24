f24a2d710e99b6d8d20aaf15cc35423610c6161c
regiuss <regiuslink@gmail.com>
2022-10-24 13:27:10 +0300

    fxml and controller design

---
+++ src/main/java/ru/regiuss/dxf/selection/helper/controller/MainController.java
@@ -0,0 +1,64 @@
+package ru.regiuss.dxf.selection.helper.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private ListView<String> opListView;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private TextField resultFolderField;
+
+    @FXML
+    private ListView<String> sizeListView;
+
+    @FXML
+    private TextField sourceFolderField;
+
+    @FXML
+    private TextField specificationFileField;
+
+    @FXML
+    private Text statusText;
+
+    @FXML
+    private ListView<String> templateListView;
+
+    @FXML
+    void onBrowseResultFolder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBrowseSourceFolder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onBrowseSpecificationFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onStart(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
