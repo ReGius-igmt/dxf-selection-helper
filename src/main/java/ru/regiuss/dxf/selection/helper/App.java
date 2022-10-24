f037bd832f9748d6db25474526f1938b0fa8c72e
regiuss <regiuslink@gmail.com>
2022-10-24 13:42:15 +0300

    save window position

--- src/main/java/ru/regiuss/dxf/selection/helper/App.java
+++ src/main/java/ru/regiuss/dxf/selection/helper/App.java
@@ -6,17 +6,44 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.prefs.Preferences;
+
 public class App extends Application {
+
+    private Stage stage;
+
     @Override
     public void start(Stage stage) throws Exception {
+        this.stage = stage;
+
         stage.setTitle("DXFSelectionHelper");
         stage.setMinWidth(800);
         stage.setMinHeight(600);
-        stage.setWidth(800);
-        stage.setHeight(600);
+
+        Preferences userPrefs = Preferences.userNodeForPackage(getClass());
+        double x = userPrefs.getDouble("stage.position.x", -1);
+        double y = userPrefs.getDouble("stage.position.y", -1);
+        double w = userPrefs.getDouble("stage.position.width", 800);
+        double h = userPrefs.getDouble("stage.position.height", 600);
+        stage.setWidth(w);
+        stage.setHeight(h);
+        if(x > 0 && y > 0) {
+            stage.setX(x);
+            stage.setY(y);
+        }
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main.fxml"));
         Parent parent = loader.load();
         stage.setScene(new Scene(parent));
         stage.show();
     }
+
+    @Override
+    public void stop() {
+        Preferences userPrefs = Preferences.userNodeForPackage(getClass());
+        userPrefs.putDouble("stage.position.x", stage.getX());
+        userPrefs.putDouble("stage.position.y", stage.getY());
+        userPrefs.putDouble("stage.position.width", stage.getWidth());
+        userPrefs.putDouble("stage.position.height", stage.getHeight());
+    }
+
 }
